87056983cac1d59b0fb22f7dc12d38026db46cee
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-11-30 16:30:21 +1100

    Updating scalatest from 3.0.8 to 3.1.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.10.3"
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.8"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
