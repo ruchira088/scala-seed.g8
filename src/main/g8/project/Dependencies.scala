cef9357de904d5670011901cbe140d33c8db0f27
Ruchira (Richie) Jayasekara <me@ruchij.com>
2019-12-15 14:55:04 +1100

    Upgrading the kind-projector to 0.11.0

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies
 {
   val SCALA_VERSION = "2.13.1"
 
-  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.10.3"
+  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
 
