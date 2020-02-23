c7fc6b265a9218daec507f0b02a28e8860b1576b
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-02-23 13:28:59 +1100

    Upgrading scalatest to 3.1.1

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.0" cross CrossVersion.full
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.0"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
