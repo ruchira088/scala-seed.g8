9bee18f9cc6b4db01755f080afc0bf26d4292615
Ruchira (Richie) Jayasekara <me@ruchij.com>
2020-05-09 17:28:06 +1000

    Upgrading to Scala Test 3.1.2

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies
 
   lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.3" cross CrossVersion.full
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.1"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.2"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
