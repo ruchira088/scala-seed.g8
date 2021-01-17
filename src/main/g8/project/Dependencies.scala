e44fc11a67132a1118119327d3047cddef7dda1e
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-01-17 13:49:36 +1100

    Upgrading compiler plugins

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -4,9 +4,9 @@ object Dependencies
 {
   val ScalaVersion = "2.13.4"
 
-  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.2" cross CrossVersion.full
+  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.3" cross CrossVersion.full
 
-  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.6" cross CrossVersion.full
+  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.7" cross CrossVersion.full
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.3"
 
