f465e187b6315004f224a8718a9013ba77541a70
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-02-27 02:27:23 +1100

    Upgrading to Scala 2.13.5

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies
 {
-  val ScalaVersion = "2.13.4"
+  val ScalaVersion = "2.13.5"
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.3" cross CrossVersion.full
 
-  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.7" cross CrossVersion.full
+  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.8" cross CrossVersion.full
 
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.5"
 
