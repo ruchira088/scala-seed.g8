6e7139023cbfc34a0025c28e97bc0f6938947af9
Ruchira (Richie) Jayasekara <me@ruchij.com>
2021-05-20 19:36:36 +1000

    Upgrading dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,13 +2,13 @@ import sbt._
 
 object Dependencies
 {
-  val ScalaVersion = "2.13.5"
+  val ScalaVersion = "2.13.6"
 
-  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.11.3" cross CrossVersion.full
+  lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.13.0" cross CrossVersion.full
 
-  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.8" cross CrossVersion.full
+  lazy val scalaTypedHoles = "com.github.cb372" % "scala-typed-holes" % "0.1.9" cross CrossVersion.full
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.8"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
