c289fe9d16505bc525321593bfc805269e7913f8
Ruchira (Richie) Jayasekara <me@ruchij.com>
2022-08-04 00:27:59 +1000

    Upgrading ScalaTest to 3.2.13

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.13.2" cross CrossVersion.full
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.12"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.13"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
