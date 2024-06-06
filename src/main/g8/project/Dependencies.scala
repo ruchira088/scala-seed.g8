b5c40a1081600e36f0b7aa059ec7f6efda6a5ff4
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-06-06 17:43:42 +1000

    Upgrading dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies
 {
-  val ScalaVersion = "2.13.13"
+  val ScalaVersion = "2.13.14"
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.13.3" cross CrossVersion.full
 
