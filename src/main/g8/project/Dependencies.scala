256f62e87943721e11c2c5a2622d8513dd913afd
Ruchira (Richie) Jayasekara <me@ruchij.com>
2024-09-21 17:36:02 +1000

    Upgrading dependencies

--- src/main/g8/project/Dependencies.scala
+++ src/main/g8/project/Dependencies.scala
@@ -2,11 +2,11 @@ import sbt._
 
 object Dependencies
 {
-  val ScalaVersion = "2.13.14"
+  val ScalaVersion = "2.13.15"
 
   lazy val kindProjector = "org.typelevel" %% "kind-projector" % "0.13.3" cross CrossVersion.full
 
-  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.18"
+  lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.19"
 
   lazy val pegdown = "org.pegdown" % "pegdown" % "1.6.0"
 }
